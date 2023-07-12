ce2d3b4c8b232eebca1f580b90f9e382b867d313
RealPark <sheer12@naver.com>
2023-07-13 00:35:33 +0900

    3번째 component는 만들어지지 않고 두번째 component까지 만들어 올림

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import { DexBlock } from "../components/DexBlock";
 import dexList from "../data/dex";
+import { BigBlock,SmallBlock } from "../components/Block";
 
 const HomePage = () => {
   const [dexes, setPostList] = useState(dexList);
 
   const handleChange = (e) => {};
-
+  //className="grid grid-cols-4 px-10 mt-10"
   return (
     <div>
       This is HomePage
-      <div className="grid grid-cols-4 px-10 mt-10">
+
+      
+      <div>
         {dexes.map((dex) => (
-          <DexBlock key={dex.id} dex={dex} />
+          //<DexBlock key={dex.id} dex={dex} />
+          <>
+          <SmallBlock dex={dex} />
+          <BigBlock dex={dex}/>
+          </>
         ))}
       </div>
     </div>
