edf7a50a46b4c354192f01d0b687d1902770c3d1
WiFiHan <mukss0909056@gmail.com>
2023-07-14 16:59:14 +0900

    [create]connecting with backend. you need axios

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DexBlock } from "../components/DexBlock";
 import dexList from "../data/dex";
+import { BigBlock,SmallBlock } from "../components/Block";
+import axios from "axios"
+import { getPosts } from "../apis/api";
 
 const HomePage = () => {
+
   const [dexes, setPostList] = useState(dexList);
+	// useEffect(() => {
+	// 	const getPostsAPI = async () => {
+	// 		const posts = await getPosts();
+  //     setPostList(posts);
+  //   };
+  //   getPostsAPI();
+	// }, []);
+  // console.log(dexes);
+
 
   const handleChange = (e) => {};
-
+  //className="grid grid-cols-4 px-10 mt-10"
   return (
     <div>
       This is HomePage
-      <div className="grid grid-cols-4 px-10 mt-10">
+
+      
+      <div>
         {dexes.map((dex) => (
-          <DexBlock key={dex.id} dex={dex} />
+          //<DexBlock key={dex.id} dex={dex} />
+          <>
+          <SmallBlock dex={dex} />
+          <BigBlock dex={dex}/>
+          
+          </>
         ))}
       </div>
     </div>
