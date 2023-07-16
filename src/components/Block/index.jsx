243eeeea2748561b9f1f377d80ac1c63fb83dc5c
RealPark <sheer12@naver.com>
2023-07-16 22:46:28 +0900

    random tag done, modal display need to be corrected

--- src/components/Block/index.jsx
+++ src/components/Block/index.jsx
@@ -1,15 +1,42 @@
 import { Link } from "react-router-dom";
 import dexList from "../../data/dex";
 import { Tag } from "./tag";
+import { useState } from "react";
+import ModalBasic from "./modalBasic";
 
 export const SmallBlock = ({ dex }) => {
   //taglist 만들어서 click시 연결 되도록
   // smallblock name에 bitblock으로 이어지는 코드 작성 요
+  const [modalOpen, setModalOpen] = useState(false);
+  const showModal = () => {
+    setModalOpen(true);
+  };
 
-  return dex.invest ? (
+
+
+function getRandom (length){
+  return Math.floor(Math.random() * (length))};
+var randomTag = [];
+if (dex.tags.length){
+  randomTag.push(getRandom(dex.tags.length))
+  randomTag.push(getRandom(dex.tags.length))
+  while(randomTag[0] == randomTag[1]){
+    randomTag[1] = getRandom(dex.tags.length)
+  }
+}
+console.log(randomTag);
+  return dex.invest ? (<>
+    {modalOpen && <ModalBasic setModalOpen={setModalOpen} className="z-50"/>}
     <div className="w-[365px] h-[250px] mx-10 my-5 p-1 items-center justify-center bg-gradient-to-br rounded from-dexname/80">
       <div className="smallblock relative flex flex-col bg-white ">
-        <div className="w-full flex flex-row flex-nowrap justify-between">
+        <div className="px-2 py-1 flex justify-between">
+          <div>
+          <button onClick={showModal}>설명 버튼</button>
+          {/* {modalOpen && <ModalBasic setModalOpen={setModalOpen} className="w-300[px]/> h-200[px] z-20 border-solid border-4 bg-black"/>} */}
+          </div>
+          <button onClick={()=> console.log("좋아요 버튼 눌림")}>좋아요 버튼</button>
+        </div>
+        <div className="w-full flex flex-row flex-wrap justify-between">
           <div className="w-1/2 flex flex-col">
             <div className="flex flex-row justify-between">
               <Link
@@ -19,30 +46,33 @@ export const SmallBlock = ({ dex }) => {
               >
                 {dex.title}
               </Link>
-              <a
-                className="italic font-semibold underline"
-                href="http://www.naver.com"
-              >
-                hyper link
-              </a>
             </div>
-            <p>{dex.description}</p>
+            <p>{dex.value}</p>
           </div>
-          <div className="flex-col">
-            {dex.tags.map((id) => (
-              <Tag id={id} dexid={dex.id} />
-            ))}
+          <div className="flex-col">{
+            randomTag.length ? (
+              <>
+              <Tag id={randomTag[0]} dexid = {dex.id}/>
+              <Tag id={randomTag[1]} dexid = {dex.id}/>
+              </>
+            ):<></>
+          }
           </div>
         </div>
         <div>
           <img src="/assets/images/lion.jpeg" alt="오류" />
         </div>
       </div>
-    </div>
-  ) : (
+    </div></>
+  ) : (<>
+    {modalOpen && <ModalBasic setModalOpen={setModalOpen} className="z-50"/>}
     <div className="w-[365px] h-[250px] mx-10 my-5 p-1 items-center justify-center bg-gradient-to-br rounded from-economy_tag/80">
       <div className="smallblock relative flex flex-col bg-white ">
-        <div className="w-full flex flex-row flex-nowrap justify-between">
+      <div className="px-2 py-1 flex justify-between">
+          <button onClick={showModal}>설명 버튼</button>
+          <button onClick={()=> console.log("좋아요 버튼 눌림")}>좋아요 버튼</button>
+        </div>
+        <div className="w-full flex flex-row flex-wrap justify-between">
           <div className="w-1/2 flex flex-col">
             <div className="flex flex-row justify-between">
               <Link
@@ -52,19 +82,17 @@ export const SmallBlock = ({ dex }) => {
               >
                 {dex.title}
               </Link>
-              <a
-                className="italic font-semibold underline"
-                href="http://www.naver.com"
-              >
-                hyper link
-              </a>
             </div>
-            <p>{dex.description}</p>
+            <p>{dex.value}</p>
           </div>
-          <div className="flex-col">
-            {dex.tags.map((id) => (
-              <Tag id={id} dexid={dex.id} />
-            ))}
+          <div className="flex-col">{
+            randomTag.length ? (
+              <>
+              <Tag id={randomTag[0]} dexid = {dex.id}/>
+              <Tag id={randomTag[1]} dexid = {dex.id}/>
+              </>
+            ):<></>
+          }
           </div>
         </div>
         <div>
@@ -72,6 +100,7 @@ export const SmallBlock = ({ dex }) => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
@@ -80,17 +109,27 @@ export const BigBlock = ({ dex }, index) => {
   //index는 이전에 클릭 된 개체의 id를 의미 dex는 Big block 내용에 들어올 dex 의미
   // index와 dex id가 같으면 이전과의 관계 필요 x
 
+    const [modalOpen, setModalOpen] = useState(false);
+  const showModal = () => {
+    setModalOpen(true);
+  };
+
   const fromhome = dex.id == index ? true : false;
   var tagDexarr = [];
   dex.tags.map((id) => {
     tagDexarr.push(dexList.find((dex) => dex.id === id));
   });
 
-  return (
+  return (<>
+    {modalOpen && <ModalBasic setModalOpen={setModalOpen} className="z-50"/>}
     <div className="flex flex-col">
       {dex.invest ? (
         <div className="self-center w-[755px] h-[460px] p-1 items-center justify-center bg-gradient-to-br rounded from-dexname/80">
-          <div className="bigblock flex flex-col bg-white">
+          <div className="bigblock relative flex flex-col bg-white">
+          <div className="px-2 py-1 flex justify-between">
+          <button onClick={showModal}>설명 버튼</button>
+          <button onClick={()=> console.log("좋아요 버튼 눌림")}>좋아요 버튼</button>
+        </div>
             <div className="flex flex-row justify-between p-5">
               <Link
                 to={"/Bigblock/" + dex.id}
@@ -99,25 +138,24 @@ export const BigBlock = ({ dex }, index) => {
               >
                 {dex.title}
               </Link>
-              <a
-                className="italic font-semibold underline"
-                href="http://www.naver.com"
-              >
-                hyper link
-              </a>
               <div className="flex flex-row">
                 {dex.tags.map((id) => (
                   <Tag id={id} dexid={dex.id} />
                 ))}
               </div>
             </div>
-            <div className="flex flex-row">current rate description</div>
+            <div className="flex flex-row">current rate value</div>
             <div>full description</div>
           </div>
         </div>
+
       ) : (
         <div className="self-center w-[755px] h-[460px] p-1 items-center justify-center bg-gradient-to-br rounded from-economy_tag/80">
-          <div className="bigblock flex flex-col bg-white">
+          <div className="bigblock relative flex flex-col bg-white">
+          <div className="px-2 py-1 flex justify-between">
+          <button onClick={showModal}>설명 버튼</button>
+          <button onClick={()=> console.log("좋아요 버튼 눌림")}>좋아요 버튼</button>
+        </div>
             <div className="flex flex-row justify-between p-5">
               <Link
                 to={"/Bigblock/" + dex.id}
@@ -126,12 +164,6 @@ export const BigBlock = ({ dex }, index) => {
               >
                 {dex.title}
               </Link>
-              <a
-                className="italic font-semibold underline"
-                href="http://www.naver.com"
-              >
-                hyper link
-              </a>
               <div className="flex flex-row">
                 {dex.tags.map((id) => (
                   <Tag id={id} dexid={dex.id} />
@@ -149,5 +181,6 @@ export const BigBlock = ({ dex }, index) => {
         ))}
       </div>
     </div>
+    </>
   );
 };
--- src/components/Block/tag.jsx
+++ src/components/Block/tag.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
 import dexList from "../../data/dex";
 import { BigBlock,SmallBlock } from "./index";
 import { Link } from "react-router-dom";
 
 // a 추후 onClick으로 대체
 export const Tag = ({id},{dexid})=>{
+
+  //getDexList 함수 필요
+
   //dexid는 tag가 붙어있는 dex의 id
+  // find 함수를 사용했으나 비동기적 사용으로 렌더링 이전에 dex 객체를 받아오지 못해 id를 인덱스로 사용해서 가져옴
     const tagid = id;
-    const dex = dexList.find(dex => dex.id === id);
-    return (
+    console.log("tagid 찾기");
+    console.log(tagid);
+    const dex = dexList[id];
+    // const dex = dexList.find(dex => dex.id == id);
+    console.log("확인");
+    console.log(dex);
+    return dex && (
       <div className="dextag">
         <Link to={'/Bigblock/'+tagid} state = {{ istag : true}}>{dex.title}</Link>
       </div>  
